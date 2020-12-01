50d31cde6709bac98f62dae98465ad926e34fb7c
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-12-01 06:47:05 +0200

    Add SearchTrend.csv

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+data = pd.read_csv(r'SearchTrend.csv')
+data.describe()
+
+z = sns.relplot(x='cold', y='coronavirus', data=data)
+
+# print(data.isnull().sum())
